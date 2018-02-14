f1d265599bccdc91e8628e2866575d28dc37f920
XCarrel <Xavier.Carrel@cpnv.ch>
2018-02-14 10:28:34 +0100

    Restructure wip

---
+++ Code/Scrasp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scrasp.Startup))]
+namespace Scrasp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
