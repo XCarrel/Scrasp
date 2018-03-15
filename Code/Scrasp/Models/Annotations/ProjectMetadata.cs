510dbaf3899b24159217a8cd0108c394f87b6ae0
LogLauncher <struanforsyth@gmail.com>
2018-03-15 10:31:49 +0100

    Incorporated metadata

---
+++ Code/Scrasp/Models/Annotations/ProjectMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scrasp.Models {
+    public class ProjectMetadata {
+        [Display(Name = "Titre")]
+        public object title { get; set; }
+
+        [Display(Name = "Description du projet")]
+        public object projectDescription { get; set; }
+
+        [Display(Name = "Repository Git")]
+        public object refRepo { get; set; }
+    }
+
+    [MetadataType(typeof(ProjectMetadata))]
+    public partial class Project { }
+}
---
+++ Code/Scrasp/Models/Annotations/StoryStateMetadata.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scrasp.Models {
+    public class StoryStateMetadata {
+        [Display(Name = "État")]
+        public object stateName { get; set; }
+    }
+
+    [MetadataType(typeof(StoryStateMetadata))]
+    public partial class StoryState { }
+}
