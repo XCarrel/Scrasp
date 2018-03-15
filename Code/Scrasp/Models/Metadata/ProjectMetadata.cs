96aaf8189ab4e8b5de0f605ddf58d400c4d2fea1
Unknown <loic.dessaules@gmail.com>
2018-03-15 10:27:15 +0100

    Added project metadata

---
+++ Code/Scrasp/Models/Metadata/ProjectMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Scrasp.Models
+{
+    public class ProjectMetadata
+    {
+
+        [Display(Name = "Titre")]
+        public object title;
+        [Display(Name = "Description")]
+        public object projectDescription;
+        [Display(Name = "Repository")]
+        public object refRepo;
+        [Display(Name = "Stories")]
+        public object Stories;
+        [Display(Name = "Teams")]
+        public object Teams;
+    }
+
+    [MetadataType(typeof(ProjectMetadata))]
+    public partial class Project
+    {
+    }
+
+}
