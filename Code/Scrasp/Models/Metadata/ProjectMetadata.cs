7a2860fffde999794bb49efda97432c29815261f
Dessauges Antoine <dessauges.antoine@gmail.com>
2018-03-20 08:09:13 +0100

    Finish fix

--- Code/Scrasp/Models/Metadata/ProjectMetadata.cs
+++ Code/Scrasp/Models/Metadata/ProjectMetadata.cs
@@ -3,6 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Scrasp.Models
 {
+    [MetadataType(typeof(ProjectMetadata))]
+    public partial class Project
+    {
+    }
+
     public class ProjectMetadata
     {
         [Display(Name = "Titre")]
