c678574d7c1d3fef6cc5620dcb235c034c06dd76
Unknown <loic.dessaules@gmail.com>
2018-03-29 09:35:20 +0200

    Added Teams and jobs  APIcontroller

---
+++ Code/Scrasp/Controllers/TeamsAPIController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Scrasp.Models;
+using System.Web.Script.Serialization;
+
+namespace Scrasp.Controllers
+{
+    public class TeamsAPIController : ApiController
+    {
+        private scraspEntities db = new scraspEntities();
+
+        // GET: api/TeamsAPI
+        public String GetTeams()
+        {
+            List<Team> myteams = db.Teams.ToList();
+
+            return new JavaScriptSerializer().Serialize(myteams);
+        }
+
+        // GET: api/TeamsAPI/5
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult GetTeam(int id)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(team);
+        }
+
+        // PUT: api/TeamsAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTeam(int id, Team team)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != team.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(team).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeamExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TeamsAPI
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult PostTeam(Team team)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Teams.Add(team);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = team.id }, team);
+        }
+
+        // DELETE: api/TeamsAPI/5
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult DeleteTeam(int id)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            db.Teams.Remove(team);
+            db.SaveChanges();
+
+            return Ok(team);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TeamExists(int id)
+        {
+            return db.Teams.Count(e => e.id == id) > 0;
+        }
+    }
+}
