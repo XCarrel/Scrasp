89b68404c4b8f4838e2291d941bcba0182fc3043
Doran Kayoumi <dorankayoumi@gmail.com>
2018-04-19 09:37:24 +0200

    Get user projects and jobs and give them to the view

--- Code/Scrasp/Controllers/DashboardController.cs
+++ Code/Scrasp/Controllers/DashboardController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Scrasp.Models;
+using System.Data.Entity;
 
 namespace Scrasp.Controllers
 {
     public class DashboardController : Controller
     {
+        private scraspEntities db = new scraspEntities();
+
         // GET: Dashboard
         public ActionResult Index()
         {
@@ -17,7 +21,29 @@ namespace Scrasp.Controllers
                 Response.Redirect("Account/Login");
             }
 
-            return View();
+            // get authenticated username
+            String authusername = System.Web.HttpContext.Current.User.Identity.Name.Split(new []{ '@' })[0];
+            ScraspUser user = db.ScraspUsers.SingleOrDefault(u => u.username == authusername);
+
+            List<Team> teams = db.Teams.Where(t => t.ScraspUsers_id == user.id).ToList();
+            List<Project> userProjects = new List<Project>();
+
+            foreach (Team team in teams)
+            {
+                // check that the project wasn't already added
+                if (!userProjects.Contains(team.Project)) {
+                    // add it
+                    userProjects.Add(team.Project);
+                }
+            }
+
+            DashboardViewModel dashboard = new DashboardViewModel
+            {
+                projects = userProjects,
+                jobs = db.Jobs.Where(j => j.ScraspUsers_id == user.id).ToList()
+            };
+
+            return View(dashboard);
         }
     }
 }
