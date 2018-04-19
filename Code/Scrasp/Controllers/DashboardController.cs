57248e191db15d3e89f6499d660811050d037947
SOTILLO Esteban <Esteban.SOTILLO@cpnv.ch>
2018-04-19 10:37:24 +0200

    end of evel commit

---
+++ Code/Scrasp/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scrasp.Models.ViewModels
+{
+    public class DashboardController : Controller
+    {
+        private scraspEntities db = new scraspEntities();
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            DashboardViewModel model = new DashboardViewModel();
+
+            model.projects = db.Projects.ToList();
+            model.users = db.ScraspUsers.ToList();
+            model.teams = db.Teams.ToList();
+            model.jobs = db.Jobs.ToList();
+            model.states = db.JobStates.ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ Code/Scrasp/Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrasp.Models
+{
+    public class DashboardViewModel
+    {
+        public Project project;
+        public ScraspUser user;
+        public List<Project> projects;
+        public List<ScraspUser> users; // all users
+        public List<Job> jobs;
+        public List<Team> teams;
+        public Job job;
+        public List<JobState> states;
+    }
+}
