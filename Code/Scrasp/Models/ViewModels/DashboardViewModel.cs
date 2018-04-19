798132ce50a142257391bc9d47bba22d48aa2ca1
Doran Kayoumi <dorankayoumi@gmail.com>
2018-04-19 08:21:33 +0200

    Create Dashboard page
    Created base controller, view and viewModel

---
+++ Code/Scrasp/Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrasp.Models
+{
+    public class DashboardViewModel
+    {
+        public List<Project> projects;
+        public List<Job> jobs;
+    }
+}
