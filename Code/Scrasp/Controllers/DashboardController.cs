cb0f8105b0a921ed609446663b8940b32d048db0
ilias <ilias.goujgali@cpnv.ch>
2018-04-19 10:39:11 +0200

    exam

---
+++ Code/Scrasp/Controllers/DashboardController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Scrasp.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Scrasp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private scraspEntities db = new scraspEntities();
+
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            // TODO find a way to get connected user id
+            var user = db.ScraspUsers.Find(1);
+            
+            // tried... doesnt work
+            TaskDashboardViewModel model = new TaskDashboardViewModel();
+
+            //var teams = db.Teams.Where(t => t.ScraspUser.id == idUser).Include(x => x.Project);
+            //var userTeams = user.Teams
+
+            //var projects = db.Projects.Where(project => userTeams.Any(team => team.id == project.Teams);
+            var jobs = db.Jobs.Where(x => x.ScraspUsers_id == user.id && x.JobState.hideInDashboard != 1).ToList();
+            ViewBag.jobs = jobs;
+
+            return View();
+         
+        }
+        public ActionResult finishTask(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            job.JobStates_id = 4;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Dashboard/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // GET: Dashboard/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,title,projectDescription,refRepo,startDate,endDate")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(project);
+        }
+
+        // GET: Dashboard/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Dashboard/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,title,projectDescription,refRepo,startDate,endDate")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(project);
+        }
+
+        // GET: Dashboard/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(project);
+        }
+
+        // POST: Dashboard/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Code/Scrasp/Models/ViewModels/TaskDashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrasp.Models
+{
+    /// <summary>
+    /// Model for the team management table
+    /// </summary>
+    public class TaskDashboardViewModel
+    {
+        public List<Job> jobs;
+    }
+}
